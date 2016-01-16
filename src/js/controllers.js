07b7a4ae6712e0531b405c226469cff0848112fc
Dustin <dustinsinkey1@gmail.com>
2016-01-15 16:42:05 -0800

    Altered controller.js to define grid

--- src/js/controllers.js
+++ src/js/controllers.js
@@ -1,4 +1,11 @@
 'use strict';
 angular.module('myApp.controllers', []).controller('NgGridCtrl',['$scope', 'dataService', function ($scope, dataService) {
-    console.log(dataService.data);
+    $scope.gridOptions = {
+        data: 'data',
+        columnDefs: [
+            {field: 'lang', displayName: 'Language', width: "45%"},
+            {field: 'year', displayName: 'Year', width: "25%"},
+            {field: 'decade', displayName: 'Decade', width: "30%"}
+        ]
+    }
 }]);
